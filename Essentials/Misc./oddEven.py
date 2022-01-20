9a098e4cb724e32bb20ed073e36d3cae6401bc71
knguyen111601 <knguyen111601@gmail.com>
2022-01-19 18:18:14 -0800

    new folder

--- Essentials/Misc./oddEven.py
+++
@@ -1,9 +0,0 @@
-def oddOrEven(n):
-    if (n%2):
-        print("Either")
-    elif (n/2 % 2):
-        print("Odd")
-    else:
-        print("Even")
-
-oddOrEven(6)
--- ShortTechnicalQuiz.js
+++
@@ -1,7 +0,0 @@
-
-let sum = 0
-
-for (var i = 1; i<=100; i+=2) {
-    sum += 1
-    console.log(sum)
-}
