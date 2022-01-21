af1267405f9b6c4c5faf8d60167248adc9e00125
knguyen111601 <knguyen111601@gmail.com>
2022-01-20 21:18:47 -0800

    valid parenthesis

--- Leetcode/ValidParenthesis.js
+++ Leetcode/ValidParenthesis.js
@@ -5,3 +5,33 @@
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
 
+const isValid = (s) => {
+    const map = {
+        '}':'{',
+        ']':'[',
+        ')':'('
+    }
+  
+    const stack = []
+    let returnValue = true;
+  
+  
+    for (let i = 0; i < s.length; i++){
+        let char = s[i];
+        if(Object.values(map).includes(char)){
+            stack.push(char)
+        }else{
+            let popped = stack.pop();
+            if(popped != map[char]){
+                returnValue = false
+            }
+        }
+    }
+    if(stack.length != 0){
+        returnValue = false
+    }
+    return returnValue
+}
+
+console.log(isValid("))"))
+
