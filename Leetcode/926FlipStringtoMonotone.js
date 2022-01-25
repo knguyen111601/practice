f98eac68e623846652816ce37f6269ef90265da5
knguyen111601 <knguyen111601@gmail.com>
2022-01-25 09:23:53 -0800

    diff solution to 926

--- Leetcode/926FlipStringtoMonotone.js
+++ Leetcode/926FlipStringtoMonotone.js
@@ -6,29 +6,56 @@
 
 // Return the minimum number of flips to make s monotone increasing.
 
-const minFlipsMonoIncr = (s) => {
+// const minFlipsMonoIncr = (s) => {
     
-    let zeros = 0
-    let ans = 0
+//     let zeros = 0
+//     let ans = 0
 
-    // Changing all 0's to 1s and recording how many steps it takes
-    for (let i = 0; i < s.length; i++){
-        if (s[i] == "0") {
-            zeros += 1
-        }
-    }
+//     // Changing all 0's to 1s and recording how many steps it takes
+//     for (let i = 0; i < s.length; i++){
+//         if (s[i] == "0") {
+//             zeros += 1
+//         }
+//     }
 
-    ans = zeros
+//     ans = zeros
+
+//     for (let i = 0; i < s.length; i++) {
+//         if (s[i] == "0") {
+//             zeros -= 1
+//         } else {
+//             zeros += 1
+//         }
+//         ans = Math.min(zeros, ans)
+//     }
+//     console.log(ans)
+// }
+
+// minFlipsMonoIncr("00110")
+
+
+const minFlipsMonoIncr = (s) => {
+    let numberFlips = 0
+    let ones = 0
 
     for (let i = 0; i < s.length; i++) {
-        if (s[i] == "0") {
-            zeros -= 1
+        if (s[i] == '0') {
+            // if it the number it is on is 0 and isn't the first one, flip to 1 so increase by 1 flip
+            if (ones !== 0) {
+                numberFlips  += 1
+            }
+        // if number is 1 add to ones count
         } else {
-            zeros += 1
+            ones += 1
         }
-        ans = Math.min(zeros, ans)
     }
-    console.log(ans)
+
+    // if the total flips is greater than the number of 1s then set total flips to number of ones
+    if (numberFlips > ones) {
+        numberFlips = ones
+    }
+
+    console.log(numberFlips)
 }
 
 minFlipsMonoIncr("00110")
