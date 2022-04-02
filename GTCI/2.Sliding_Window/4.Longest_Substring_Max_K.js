4fa00e299b59ba92ae4aa4ca7113e519b1dec574
knguyen111601 <knguyen111601@gmail.com>
2022-04-01 21:33:20 -0700

    committing

---
+++ GTCI/2.Sliding_Window/4.Longest_Substring_Max_K.js
@@ -0,0 +1,19 @@
+// Longest Substring with maximum K Distinct Characters (medium)
+
+// Given a string, find the length of the longest substring in it with no more than K distinct characters.
+
+const longestSubstring = (str, k) => {
+
+    let current = 0
+    let currentMax = 0
+    windowStart = 0
+
+
+    
+
+
+
+
+
+}
+
