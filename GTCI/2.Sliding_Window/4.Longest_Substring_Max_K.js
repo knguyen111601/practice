e268c534f8c7bba203e7b53e86c28f44c8a89778
knguyen111601 <knguyen111601@gmail.com>
2022-04-07 09:26:20 -0700

    longest substring with max k distinct characters

--- GTCI/2.Sliding_Window/4.Longest_Substring_Max_K.js
+++ GTCI/2.Sliding_Window/4.Longest_Substring_Max_K.js
@@ -1,31 +1,41 @@
 // Longest Substring with maximum K Distinct Characters (medium)
 
-// Given a string, find the length of the longest substring in it with no more than K distinct characters.
+// Given a string, find the length of the longest substring in it with no more than K distinct characters
 
 const longestSubstring = (str, k) => {
 
-    let current = 0
+    let windowStart = 0
     let currentMax = 0
-    windowStart = 0
+    let frequency = {}
 
-    for (let i = 0; i < str.length; i++){
+    for (let i = 0; i < str.length; i++) {
+      // save current item to variable
+      const current = str[i]
+      // if the item does not exist in the hash map, add it 
+      if (!(current in frequency)) {
+        frequency[current] = 0
+      }
+      // add to count in hash map
+      frequency[current] += 1
 
-        current += str[i]
-
-
-        if (i >= k - 1) {
-            currentMax = Math.max(current, currentMax)
-            current -= str[windowStart]
-            windowStart += 1
+      // Shrink the sliding window, until we are left with "k" distinct characters in the frequency 
+      while (Object.keys(frequency).length > k) {
+        // save first item in window
+        const start = str[windowStart]
+        // remove it from the frequency count
+        frequency[start] -= 1
+        // if the frequency count of that character becomes 0, remove it from the hash map
+        if (frequency[start] === 0) {
+          delete frequency[start]
         }
+        // move forward 1 in the window
+        windowStart += 1 // shrink the window
+      }
+      // compare max of currentMax to the size of the window
+      currentMax = Math.max(currentMax, i - windowStart + 1)
     }
-    
+
     return currentMax
-
-
-
-
 }
 
-console.log(longestSubstring([2,1,5,1,3,2], 3))
-
+console.log(longestSubstring("abracadabra", 3))
