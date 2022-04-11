e91b5e4bcf802939ea37e8d255c92db2c2fd5f32
knguyen111601 <knguyen111601@gmail.com>
2022-04-11 09:47:19 -0700

    Hoisting for functions

--- Essentials/Hoisting/1.Hoisting.js
+++ Essentials/Hoisting/1.Hoisting.js
@@ -21,3 +21,21 @@ console.log(e + " " + f)
 var f 
 
 ////////////////////////////////////////////////////////////////////////////////////////////
+// This will hoist the entire function to the top of the scope and run properly
+// This console.log will log 10
+console.log(example())
+
+function example() {
+    var a = 10
+    return a
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// Downside of hoisting 
+// Take up space inside the browser and slow down the code
+// Takes space for hoisting before anything can be loaded in the browser
+
+// Methods to avoid
+// Declare variables inside of functions that are used inside of the functions only
+// Can assign variables to functions rather than having a named function that needs to be hoisted
