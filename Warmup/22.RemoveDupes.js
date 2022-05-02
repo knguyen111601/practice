ef993d68e81d072fa3b90baa4079c67c3d866885
knguyen111601 <knguyen111601@gmail.com>
2022-05-02 16:03:31 -0700

    remove dupe

--- Warmup/22.RemoveDupes.js
+++ Warmup/22.RemoveDupes.js
@@ -3,17 +3,19 @@
 // Given an array of sorted numbers, remove all duplicates from it. You should not use any extra space; after removing the duplicates in-place return the length of the subarray that has no duplicate in it.
 
 const removeDupes = (arr) => {
+
+    let left = 0
     let nextNonDupe = 1
 
-    let i = 0
-    while (i < arr.length) {
-        if (arr[nextNonDupe - 1] !== arr[i]) {
-            arr[nextNonDupe] = arr[i]
+    while (left < arr.length) {
+
+        if (arr[nextNonDupe-1] !== arr[left]) {
+            arr[nextNonDupe] = arr[left] 
             nextNonDupe += 1
         }
-        i+=1
+        left += 1
     }
-
     return nextNonDupe
 }
 
+console.log(removeDupes([2,3,3,3,6,9,9]))
