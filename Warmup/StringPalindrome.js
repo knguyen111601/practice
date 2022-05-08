25ff0cb37b4a56678c7a85b14b9341d493661aaa
knguyen111601 <knguyen111601@gmail.com>
2022-05-08 12:15:32 -0700

    isPalindrome string

---
+++ Warmup/StringPalindrome.js
@@ -0,0 +1,13 @@
+const isPalindrome = (str) => {
+
+    let reversed = ""
+
+    for (let i = str.length-1; i >= 0; i--) {
+        reversed += str[i]
+    }
+
+    return reversed === str
+}
+
+console.log(isPalindrome("racecar"))
+console.log(isPalindrome("race"))
