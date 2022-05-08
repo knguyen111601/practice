ed87a75c11600cd489f374c623a09ccfe1460621
knguyen111601 <knguyen111601@gmail.com>
2022-05-08 12:25:37 -0700

    cyclic sort introduction

---
+++ GTCI/6.Cyclic_Sort/1.Introduction/1.Intro.js
@@ -0,0 +1,3 @@
+// Introduction
+// This pattern describes an interesting approach to deal with problems involving arrays containing numbers in a given range. For example, take the following problem:
+    // You are given an unsorted array containing n numbers taken from the range 1 to n. The array can have duplicates, which means that some numbers will be missing. Find all the missing numbers.
