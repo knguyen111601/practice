076169a198c67c313b5eed50b846f1b613605560
knguyen111601 <knguyen111601@gmail.com>
2022-05-09 05:54:09 -0700

    intro

---
+++ GTCI/10.Topological_Sort/Introduction/Intro.js
@@ -0,0 +1,5 @@
+// Introduction 
+
+// Topological sort is used to find a linear ordering of elements that have dependencies on each other. For example, if event "B" is dependent of event "A", "A" comes before "A" in topological order
+
+// This pattern defines an easy way to understand the technique for performing topological sorting of a set of elements and then solves a few problems using it
