057403e0c477e7e6b83a976c9a86f7715c741ef7
knguyen111601 <knguyen111601@gmail.com>
2022-05-08 21:36:53 -0700

    intro

---
+++ GTCI/9.TreeDepthFirstSearch/Introduction/Introduction.js
@@ -0,0 +1,7 @@
+// Introduction 
+
+// This pattern is based on the Depth First Search (DFS) technique to traverse a tree
+
+// We will be using recursion (or we can also use a stack for the iterative approach) to keep track of all the previous (parent) nodes while traversing
+
+// The Space Complexity will be O(H) where H is the max height of the tree
